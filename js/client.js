3817e8e51abd6b89411f53225de25bc5fc38790a
Gradask <93795517+Gradask@users.noreply.github.com>
2025-01-29 19:33:13 +0100

    Update client.js

--- js/client.js
+++ js/client.js
@@ -61,7 +61,7 @@ const init = () => {
   // Input
   const inputContainer = document.getElementById("input-container");
   const input = document.getElementById("input");
-  input.textContent = "";
+  input.value = "";
 
   // Settings
   const settings = document.getElementById("settings");
