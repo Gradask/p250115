c2bdf671c13f604bee236a6d501c23890a5b60df
Gradask <93795517+Gradask@users.noreply.github.com>
2025-01-30 02:54:44 +0100

    Update shaders.js

--- js/shaders.js
+++ js/shaders.js
@@ -81,8 +81,7 @@ const tex = {
 
     void main() {
       vec2 uvScale = vec2(16.0 / u_texSize.x, 16.0 / u_texSize.y);
-      //vec2 uv = v_texcoord + (gl_PointCoord * uvScale); // Adjust UV mapping
-      vec2 uv = v_texcoord + (gl_PointCoord * (uvScale - vec2(1.0) / u_texSize));
+      vec2 uv = v_texcoord + (gl_PointCoord * uvScale); // Adjust UV mapping
       
       vec4 texColor = texture(u_texture, uv);
       
