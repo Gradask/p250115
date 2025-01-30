ae84a20ee11b17f9cd37f118f8298623f1a012df
Gradask <93795517+Gradask@users.noreply.github.com>
2025-01-30 03:50:52 +0100

    Update TexRenderer.js

--- js/TexRenderer.js
+++ js/TexRenderer.js
@@ -19,7 +19,8 @@ class TexRenderer {
       TEXTURE_MAG_FILTER: "NEAREST"
     });
     let tex2 = await glhelpers.setupTexture(gl, "img/font1.png");
-    let tex3 = await glhelpers.setupTexture(gl, "mesh/Map__45_Normal_Bump.png");
+    let tex3 = await glhelpers.setupTexture(gl, "img/font2.png");
+    let tex4 = await glhelpers.setupTexture(gl, "mesh/Map__45_Normal_Bump.png");
   
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, tex1);
@@ -29,6 +30,9 @@ class TexRenderer {
   
     gl.activeTexture(gl.TEXTURE2);
     gl.bindTexture(gl.TEXTURE_2D, tex3);
+
+    gl.activeTexture(gl.TEXTURE3);
+    gl.bindTexture(gl.TEXTURE_2D, tex4);
     this.isReady = true;
   }
 
