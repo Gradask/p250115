93567a87ab0e0a4a500f2899122fea323d686420
Gradask <93795517+Gradask@users.noreply.github.com>
2025-02-06 01:50:00 +0100

    Update MeshRenderer.js

--- js/MeshRenderer.js
+++ js/MeshRenderer.js
@@ -18,33 +18,34 @@ class MeshRenderer {
 
     glhelpers.setupBuffers(gl, this.programInfo.attribs, drawInfo);
 
-    let mvMat = mat4helpers.multiply(camera.vMat, drawInfo.mMat);
-    let mvpMat = mat4helpers.multiply(camera.pMat, mvMat);
+    if (camera.isDirty || drawInfo.isDirty) {
+      let mvMat = mat4helpers.multiply(camera.vMat, drawInfo.mMat);
+      let mvpMat = mat4helpers.multiply(camera.pMat, mvMat);
 
-    let inverseMvMat = mat4helpers.inverse(mvMat);
-    let nMat = mat4helpers.transpose(inverseMvMat);
-    const u_normalMatrix = [
-      nMat[0], nMat[1], nMat[2],
-      nMat[4], nMat[5], nMat[6],
-      nMat[8], nMat[9], nMat[10]
-    ]; 
+      let inverseMvMat = mat4helpers.inverse(mvMat);
+      let nMat = mat4helpers.transpose(inverseMvMat);
+      drawInfo.u_normalMatrix = [
+        nMat[0], nMat[1], nMat[2],
+        nMat[4], nMat[5], nMat[6],
+        nMat[8], nMat[9], nMat[10]
+      ]; 
 
-    drawInfo.u_matrix = mvpMat;
-    drawInfo.u_normalMatrix = u_normalMatrix;
-    drawInfo.u_lightDirection = light.normalizedDir;
+      drawInfo.u_matrix = mvpMat;
+      drawInfo.u_lightDirection = light.normalizedDir;
+      drawInfo.count = drawInfo.indices.length;
+    }
 
     glhelpers.setupUniforms(gl, this.programInfo.uniforms, drawInfo);
     glhelpers.setupIndices(gl, drawInfo);
 
-    const count = drawInfo.indices.length;
-
     gl.drawElements(
       gl.TRIANGLES,
-      count,
+      drawInfo.count,
       gl.UNSIGNED_SHORT,
       0
     );
-  }
+
+    drawInfo.isDirty = false;
 }
 
 export default MeshRenderer;
