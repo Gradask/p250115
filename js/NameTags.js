5ec3e4ab31a08f253f1d0c728e29e16559707e0a
Gradask <93795517+Gradask@users.noreply.github.com>
2025-02-06 01:04:08 +0100

    Update NameTags.js

--- js/NameTags.js
+++ js/NameTags.js
@@ -18,35 +18,49 @@ class NameTags {
   }
 
   generateTags(kernels, generateColors) {
-    this.sanitizedNames = {};
-    const chars = { a_texcoord: [], a_worldOffset: [], a_color: [] };
-    
-    for (const kernel of kernels) {
-      let sanitized = "";
-      const name = kernel.name.toLowerCase();
-      for (let i = 0; i < name.length; i++) sanitized += this.sanitizeString(name[i]);
-      this.sanitizedNames[kernel.name] = sanitized;
-      
-      for (let i = 0; i < sanitized.length; i++) {
-        const char = sanitized[i];
-        const item = this.fontInfo.items[char];
-        if (!item) continue;
+    this.sanitizedNames = {}; // Store preprocessed names
+    this.chars = { a_texcoord: [], a_worldOffset: [], a_color: [] };
   
-        const { a_texcoord, a_worldOffset } = this.generateTexData({i, ...this.fontInfo, ...item });
-        chars.a_texcoord.push(a_texcoord);
-        chars.a_worldOffset.push(a_worldOffset);
-        if (generateColors) chars.a_color.push(kernel.tagColor);
+    for (const kernel of kernels) {
+      const sanitized = this.getValidCharacters(kernel.name);
+      this.sanitizedNames[kernel.name] = sanitized;
+  
+      this.processName(sanitized, kernel, generateColors);
+    }
+  
+    // Flatten arrays at the end instead of inside loops
+    this.attribs.a_texcoord.data = this.chars.a_texcoord.flat();
+    this.relOffsets = this.chars.a_worldOffset;
+    if (generateColors) this.attribs.a_color.data = this.chars.a_color.flat();
+  }  
+  
+  getValidCharacters(name) {
+    let sanitized = "";
+    const lowerName = name.toLowerCase();
+  
+    for (let i = 0; i < lowerName.length; i++) {
+      const char = this.sanitizeString(lowerName[i]);
+      if (this.fontInfo.items[char]) {
+        sanitized += char; // Only keep valid characters
       }
     }
+  
+    return sanitized;
+  }  
 
-    this.chars = chars;
-    this.attribs.a_texcoord.data = chars.a_texcoord.flat();
-    this.relOffsets = chars.a_worldOffset;
-    if (generateColors) this.attribs.a_color.data = chars.a_color.flat();
+  processName(sanitized, kernel, generateColors) {
+    for (let i = 0; i < sanitized.length; i++) {
+      const item = this.fontInfo.items[sanitized[i]];
+      const { a_texcoord, a_worldOffset } = this.generateTexData({ i, ...this.fontInfo, ...item });
+  
+      this.chars.a_texcoord.push(a_texcoord);
+      this.chars.a_worldOffset.push(a_worldOffset);
+      if (generateColors) this.chars.a_color.push(kernel.tagColor);
+    }
   }
 
   generateTexData(data) {
-    const { i, tw, th, w, u, v } = data;
+    const { i, tw, th, u, v } = data;
   
     const u1 = u / tw;
     const v1 = v / th;
@@ -66,31 +80,31 @@ class NameTags {
   }
 
   updateRenderables(popcorns) {
-    this.attribs.a_position.data = [];
-    this.attribs.a_worldOffset.data = [];
+    const posData = this.attribs.a_position.data = [];
+    const offsetData = this.attribs.a_worldOffset.data = [];
     this.count = 0;
-    
-    for (const popcorn of popcorns.all) {
-      let [x, y, z] = popcorn.position;
-      if (mode === "time" && !popcorn.blinkState) z += 10000;
-      const absOffset = popcorn.buffer;
-      const sanitized = this.sanitizedNames[popcorn.name] || popcorn.name.toLowerCase();
-      
-      for (let char of sanitized) {
-        const item = this.fontInfo.items[char];
-        if (!item) continue;
-        this.attribs.a_position.data.push(x, y, z);
 
+    for (let i = 0, len = popcorns.all.length; i < len; i++) {
+      const popcorn = popcorns.all[i];
+      let [x, y, z] = popcorn.position;
+      if (mode === "time" && !popcorn.blinkState) {
+        z += 10000;
+      }
+      const sanitized = this.sanitizedNames[popcorn.name] || "";
+      const absOffset = popcorn.buffer;
+  
+      for (let j = 0, slen = sanitized.length; j < slen; j++) {
+        posData.push(x, y, z);
         const relOffset = this.relOffsets[this.count];
-        this.attribs.a_worldOffset.data.push(
-          absOffset[0] + relOffset[0], // x
-          absOffset[1] + relOffset[1], // y
+        offsetData.push(
+          relOffset[0] + absOffset[0], // x
+          relOffset[1] + absOffset[1] // y
         );
-        
+
         this.count++;
       }
     }
-  }
+  } 
 
   updateSize(kernels, setting) {
     const scaleFactor = parseFloat(setting.replace("x", ""));
