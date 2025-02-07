70e2e8968472c2ec1307996c07a4fd0379c6ae0c
Gradask <93795517+Gradask@users.noreply.github.com>
2025-02-07 05:41:38 +0100

    Update NameTags.js

--- js/NameTags.js
+++ js/NameTags.js
@@ -1,5 +1,3 @@
-import { mode } from "./client.js";
-
 class NameTags {
   constructor(position, pointSize, texSize, fontInfo) {
     this.position = position;
@@ -11,52 +9,46 @@ class NameTags {
 
     this.attribs = {
       a_position: { data: [] },
-      a_color: {},
-      a_texcoord: {},
-      a_worldOffset: {}
+      a_color: { data: [] },
+      a_texcoord: { data: [] },
+      a_worldOffset: { data: [] }
     }
     this.isDirty = true;
   }
 
-  generateTags(kernels, generateColors) {
-    this.sanitizedNames = {}; // Store preprocessed names
-    this.chars = { a_texcoord: [], a_worldOffset: [], a_color: [] };
+  generateTags(popcorns) {
+    this.sanitizedNames = {};
   
-    for (const kernel of kernels) {
-      const sanitized = this.getValidCharacters(kernel.name);
-      this.sanitizedNames[kernel.name] = sanitized;
-  
-      this.processName(sanitized, kernel, generateColors);
+    for (let i = 0; i < popcorns.length; i++) {
+      const popcorn = popcorns[i];
+      const sanitized = this.getValidCharacters(popcorn.name);
+      this.sanitizedNames[popcorn.name] = sanitized;
+      this.processName(sanitized, popcorn);
     }
-  
-    // Flatten arrays at the end instead of inside loops
-    this.attribs.a_texcoord.data = this.chars.a_texcoord.flat();
-    this.relOffsets = this.chars.a_worldOffset;
-    if (generateColors) this.attribs.a_color.data = this.chars.a_color.flat();
   }  
   
   getValidCharacters(name) {
     let sanitized = "";
-    const lowerName = name.toLowerCase();
+    let lowerName = name.toLowerCase();
   
     for (let i = 0; i < lowerName.length; i++) {
       const char = this.sanitizeString(lowerName[i]);
-      if (this.fontInfo.items[char]) {
-        sanitized += char; // Only keep valid characters
-      }
+      if (char === " ") sanitized += char;
+      if (this.fontInfo.items[char]) sanitized += char;
     }
   
     return sanitized;
   }  
 
-  processName(sanitized, kernel, generateColors) {
+  processName(sanitized, popcorn) {
+    popcorn.tagCoords = [];
+    popcorn.relTagOffsets = [];
+    
     for (let i = 0; i < sanitized.length; i++) {
       const item = this.fontInfo.items[sanitized[i]];
       const { a_texcoord, a_worldOffset } = this.generateTexData({ i, ...this.fontInfo, ...item });
-  
-      this.chars.a_texcoord.push(a_texcoord);
-      this.chars.a_worldOffset.push(a_worldOffset);
-      if (generateColors) this.chars.a_color.push(kernel.tagColor);
+      popcorn.tagCoords.push(a_texcoord);
+      popcorn.relTagOffsets.push(a_worldOffset);
     }
   }
 
@@ -80,38 +72,58 @@ class NameTags {
     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^\x00-\x7F]/g, "");
   }
 
-  updateRenderables(popcorns) {
-    const posData = this.attribs.a_position.data = [];
-    const offsetData = this.attribs.a_worldOffset.data = [];
-    this.count = 0;
-
-    for (let i = 0, len = popcorns.all.length; i < len; i++) {
-      const popcorn = popcorns.all[i];
-      let [x, y, z] = popcorn.position;
-      if (mode === "time" && !popcorn.blinkState) {
-        z += 10000;
-      }
-      const sanitized = this.sanitizedNames[popcorn.name] || "";
-      const absOffset = popcorn.buffer;
-  
-      for (let j = 0, slen = sanitized.length; j < slen; j++) {
-        posData.push(x, y, z);
-        const relOffset = this.relOffsets[this.count];
-        offsetData.push(
-          relOffset[0] + absOffset[0], // x
-          relOffset[1] + absOffset[1] // y
-        );
-
-        this.count++;
-      }
-    }
-  } 
-
-  updateSize(kernels, setting) {
+  updateSize(pops, setting, popcorns) {
     const scaleFactor = parseFloat(setting.replace("x", ""));
     this.u_pointSize = this.baseSize * scaleFactor;
     this.u_texture = this.u_pointSize === 8 ? 1 : 2;
-    this.generateTags(kernels);
+    this.generateTags(pops);
+    this.resetRenderables(popcorns);
+  }
+
+  resetRenderables(popcorns) {
+    this.emptyRenderables();
+    const filtered = popcorns.all.filter(p => p.state !== "disappeared");
+    for (let i = 0; i < filtered.length; i++) {
+      this.updateRenderables(filtered[i]);
+    }
+  }
+
+  emptyRenderables() {
+    this.attribs.a_position.data.length = 0;
+    this.attribs.a_worldOffset.data.length = 0;
+    this.attribs.a_color.data.length = 0;
+    this.attribs.a_texcoord.data.length = 0;
+    this.count = 0;
+  }
+
+  updateRenderables(popcorn) {
+    const sanitized = this.sanitizedNames[popcorn.name] || "";
+
+    for (let j = 0; j < sanitized.length; j++) {
+      this.attribs.a_position.data.push(
+        popcorn.position[0],
+        popcorn.position[1],
+        popcorn.position[2]
+      );
+
+      this.attribs.a_worldOffset.data.push(
+        popcorn.buffer[0] + popcorn.relTagOffsets[j][0],
+        popcorn.buffer[1] + popcorn.relTagOffsets[j][1],
+      );
+      
+      this.attribs.a_color.data.push(
+        popcorn.tagColor[0],
+        popcorn.tagColor[1],
+        popcorn.tagColor[2]
+      );
+
+      this.attribs.a_texcoord.data.push(
+        popcorn.tagCoords[j][0],
+        popcorn.tagCoords[j][1]
+      );
+
+      this.count++;
+    }
   }
 }
 
