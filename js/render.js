be4237fb2a195872b73bb5877bff6ddeabc847ca
Gradask <93795517+Gradask@users.noreply.github.com>
2025-09-06 21:53:52 +0200

    Update render.js

--- js/render.js
+++ js/render.js
@@ -92,10 +92,12 @@ window.addEventListener("resize", () => {
   backgroundFbo = glhelpers.createBackgroundFbo(gl, backgroundTexture, depthBuffer);
 
   fb.ready = false;
+  renderBackgroundSnapshot();
 });
 
 const light = new Light();
 
 export { fb, render, gl, light, texRenderer };
+
 
 
