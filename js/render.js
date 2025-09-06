21abca698ff7907732623d52aa54ca0134218469
Gradask <93795517+Gradask@users.noreply.github.com>
2025-09-06 06:54:04 +0200

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,26 +1,36 @@
-import { basic, mesh, tex } from "./shaders.js";
+import { basic, labels, mesh, tex } from "./shaders.js";
 import glhelpers from "./glhelpers.js";
 import { saucepan, popcorns, nameTags, mode, camera, target, bottomPlate } from "./client.js";
 import TexRenderer from "./TexRenderer.js";
 import MeshRenderer from "./MeshRenderer.js";
 import Light from "./Light.js";
 import BasicRenderer from "./BasicRenderer.js";
+import LabelRenderer from "./LabelRenderer.js";
 
-const canvas = document.getElementById('canvas');
-const gl = canvas.getContext('webgl2', { alpha: true });
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl2", { alpha: true });
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-gl.clearColor(0, 0, 0, 0.0);
+gl.clearColor(...glhelpers.rgb(248, 202, 212), 1);
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-const basicProgram = glhelpers.createProgram(gl,basic.vs, basic.fs);
-const basicRenderer = new BasicRenderer(gl, basicProgram, basic); 
+const basicProgram = glhelpers.createProgram(gl, basic.vs, basic.fs);
+const basicRenderer = new BasicRenderer(gl, basicProgram, basic);
 
 const texProgram = glhelpers.createProgram(gl, tex.vs, tex.fs);
 const texRenderer = new TexRenderer(gl, texProgram, tex);
 
+const labelsProgram = glhelpers.createProgram(gl, labels.vs, labels.fs);
+const labelRenderer = new LabelRenderer(gl, labelsProgram, labels);
+
 const meshProgram = glhelpers.createProgram(gl, mesh.vs, mesh.fs);
 const meshRenderer = new MeshRenderer(gl, meshProgram, mesh);
+
+let fbReady = false;
+
+let backgroundTexture = glhelpers.createBackgroundTexture(gl, canvas.width, canvas.height);
+let depthBuffer = glhelpers.createDepthBuffer(gl, canvas.width, canvas.height);
+let backgroundFbo = glhelpers.createBackgroundFbo(gl, backgroundTexture, depthBuffer);
 
 function render() {
   gl.enable(gl.BLEND);
@@ -29,18 +39,40 @@ function render() {
   gl.disable(gl.DEPTH_TEST);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  gl.disable(gl.DEPTH_TEST);
+
   if (mode === "distance" && target.isReady) basicRenderer.render(target);
   gl.enable(gl.CULL_FACE);
   if (bottomPlate.isReady) meshRenderer.render(bottomPlate);
   gl.enable(gl.DEPTH_TEST);
   if (saucepan.isReady) meshRenderer.render(saucepan);
+
   if (popcorns.all.length > 0) {
+    if (bottomPlate.isReady && saucepan.isReady && !fbReady) {
+      renderBackgroundSnapshot();
+      fbReady = true;
+    }
     gl.depthMask(false);
     texRenderer.render(popcorns);
     gl.disable(gl.DEPTH_TEST);
-    texRenderer.render(nameTags);
+    labelRenderer.render(nameTags, backgroundTexture);
   }
   camera.isDirty = false;
+}
+
+function renderBackgroundSnapshot() {
+  gl.bindFramebuffer(gl.FRAMEBUFFER, backgroundFbo);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
+
+  if (bottomPlate.isReady) meshRenderer.render(bottomPlate);
+  if (saucepan.isReady) meshRenderer.render(saucepan);
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  gl.viewport(0, 0, canvas.width, canvas.height);
 }
 
 window.addEventListener("resize", () => {
@@ -50,6 +82,13 @@ window.addEventListener("resize", () => {
   camera.updateViewport();
   camera[mode]();
   camera.getMat();
+
+  // Recreate fb
+  backgroundTexture = glhelpers.createBackgroundTexture(gl, canvas.width, canvas.height);
+  depthBuffer = glhelpers.createDepthBuffer(gl, canvas.width, canvas.height);
+  backgroundFbo = glhelpers.createBackgroundFbo(gl, backgroundTexture, depthBuffer);
+
+  fbReady = false;
 });
 
 const light = new Light();
